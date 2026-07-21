fa36a824df2d5538576ce0a627f0e3721627f033
Abu <abu@Abus-MacBook-Air.local>
2026-07-21 23:44:34 +0530

    Fix ALLOWED_HOSTS for Render

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-37lps$i=!t(hobun0z$-ta%lcoq+9iu0n+1u_iu6g(^2*wf%oy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "stockportfolio-backend.onrender.com",
+]
 
 
 # Application definition
