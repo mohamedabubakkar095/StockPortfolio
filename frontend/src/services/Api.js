5802aa0e3ee45f90d3730e06f68ee64262d052ca
Abu <abu@Abus-MacBook-Air.local>
2026-07-22 16:01:01 +0530

    Connect frontend to Render backend

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://stockportfolio-backend.onrender.com/api/",
+ baseURL: "https://stockportfolio-gm75.onrender.com/api/",
 });
 
 API.interceptors.request.use(
